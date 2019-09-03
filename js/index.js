fcee6ad35e42cff72774802942ecc9b23d84cf29
zacharybaca <baca.zach@icloud.com>
2019-09-03 16:34:32 -0500

    changed cta text

--- js/index.js
+++ js/index.js
@@ -40,3 +40,6 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let ctaTextH1 = document.querySelector('.cta-text h1');
+ctaTextH1.textContent = siteContent.cta.h1;
